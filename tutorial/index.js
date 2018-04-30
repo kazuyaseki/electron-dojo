4f9f414d691e5cd5dceaec10ab9a51d6d5656059
kazuya-seki <track613armory@gmail.com>
2018-04-30 16:44:06 +0900

    importing other files

--- tutorial/index.js
+++ tutorial/index.js
@@ -3,6 +3,12 @@ const path = require('path');
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({
+    show: false
+  });
   mainWindow.loadURL(path.join('file://', __dirname, 'index.html'));
+  // 準備できてから見せることによってネイティブアプリっぽさを演出する
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show();
+  });
 });
